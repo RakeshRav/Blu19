9dba882b04c2842a09bb9259c011ad949a037816
SangeetManghnani <sagarmanghnani@gmail.com>
2018-11-10 11:51:44 +0530

    fineeeeesh

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -20,18 +20,21 @@
 		});
 	}
 	
+	if(isExists('#normal-countdown')){
+		setInterval(function() { makeTimer(); }, 1000);
+	}
 	//NORMAL TIMES COUNTDOWN
 	
-	if(isExists('#normal-countdown')){
-		var date = $('#normal-countdown').data('date');
-		$('#normal-countdown').countdown(date, function(event) {
-			var $this = $(this).html(event.strftime(''
-				+ '<div class="time-sec"><h3 class="main-time">%D</h3> <span>Days</span></div>'
-				+ '<div class="time-sec"><h3 class="main-time">%H</h3> <span>Hours</span></div>'
-				+ '<div class="time-sec"><h3 class="main-time">%M</h3> <span>Mins</span></div>'
-				+ '<div class="time-sec"><h3 class="main-time">%S</h3> <span>Sec</span></div>'));
-		});
-	}
+	// if(isExists('#normal-countdown')){
+	// 	var date = $('#normal-countdown').data('date');
+	// 	$('#normal-countdown').countdown(date, function(event) {
+	// 		var $this = $(this).html(event.strftime(''
+	// 			+ '<div class="time-sec"><h3 class="main-time">%D</h3> <span>Days</span></div>'
+	// 			+ '<div class="time-sec"><h3 class="main-time">%H</h3> <span>Hours</span></div>'
+	// 			+ '<div class="time-sec"><h3 class="main-time">%M</h3> <span>Mins</span></div>'
+	// 			+ '<div class="time-sec"><h3 class="main-time">%S</h3> <span>Sec</span></div>'));
+	// 	});
+	// }
 	
 	
 	$('a[href="#"]').on('click', function(event){
@@ -117,6 +120,32 @@ function isExists(elem){
 		return true;
 	}
 	return false;
+}
+
+function makeTimer() {
+
+	var endTime = new Date("31 Novemeber 2018 9:56:00 GMT+01:00");			
+	endTime = (Date.parse(endTime) / 1000);
+
+	var now = new Date();
+	now = (Date.parse(now) / 1000);
+
+	var timeLeft = endTime - now;
+
+	var days = Math.floor(timeLeft / 86400); 
+	var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+	var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+	var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+	if (hours < "10") { hours = "0" + hours; }
+	if (minutes < "10") { minutes = "0" + minutes; }
+	if (seconds < "10") { seconds = "0" + seconds; }
+
+	$("#days").html('<h3 class="main-time">'+ days + '</h3><span>Days</span>');
+	$("#hours").html('<h3 class="main-time">' + hours + '</h3><span>Hours</span>');
+	$("#minutes").html('<h3 class="main-time">' + minutes + '</h3><span>Minutes</span>');
+	$("#seconds").html('<h3 class="main-time">' + seconds + '</h3><span>Seconds</span>');		
+
 }
 
 function initMap() {
